6bc43559bdd4e345fbdbc0f77a94d28df6f00ad6
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-20 13:49:50 +0200

    Create ReportController.js

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,12 @@
+import { ReportSchema } from "../models/Report.js";
+
+const GetReports = async function(req, res){
+    try{
+        const UserReports = await ReportSchema.find({});
+        res.status(200).json({MyReports: UserReports});
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+export { GetReports };
