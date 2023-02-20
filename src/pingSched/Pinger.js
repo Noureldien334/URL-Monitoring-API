214d227175ebe6ed48fc6e5fc7f5b7f19a90342b
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-21 01:25:51 +0200

    Update Pinger.js

--- src/pingSched/Pinger.js
+++ src/pingSched/Pinger.js
@@ -17,11 +17,18 @@ async function UpdateDB(Response){
       CreatorId: Response.UserId,
       URL: Response.URL
   };
+
+  const OldData = await ReportSchema.findOne({CreatorId: query.CreatorId, URL: query.URL});
   
-  await ReportSchema.findOneAndUpdate(
-      query,
-      { $set : {Status:Response.StatusText} }
-  );
+  const NewData = await ReportSchema.updateOne({CreatorId: query.CreatorId, URL: query.URL},
+                                              { $set: {Status: Response.StatusText,
+                                                       ResponseTime : Response.ResponseTime,
+                                                       UpTime: (Response.UpTime + OldData.UpTime) ,
+                                                       DownTime: Response.DownTime + OldData.DownTime,
+                                                       Outage: Response.Outage + OldData.Outage,
+                                                       Availability: ((Response.UpTime + OldData.UpTime) / (Response.UpTime + OldData.UpTime + Response.DownTime + OldData.DownTime)) * 100
+                                                }}
+                                              );
   
 }
 
@@ -29,25 +36,48 @@ async function UpdateDB(Response){
 async function Ping(){
   const ChecksToBePinged = await FetchChecks();
   
-  ChecksToBePinged.forEach( (Check) => {
+  ChecksToBePinged.forEach( async (Check) => {
 
-    setInterval( () => {
+   await setInterval( () => {
       // Making a get request for the Check's Url with the Specified Configurations
       //axios(Url,{Configs});
+
+      //Calculating Response Time
+      
+      const StartTime =  Date.now();
       axios.get(Check.Url,{
         method: 'get',
         url: Check.Url,
-        timeout: Check.timeout,
+        timeout: Check.Timeout,
       })
       .then(async (response) => { 
         //In case it's up, the response is here
-        await UpdateDB({UserId: Check.UserId, URL: Check.Url, StatusText: 'up'});
+        const EndTime = Date.now();
+        const ResponseTime = EndTime-StartTime;
+        await UpdateDB({UserId: Check.UserId,
+                        URL: Check.Url,
+                        StatusText: 'up',
+                        ResponseTime: ResponseTime,
+                        UpTime: ResponseTime / 1000,
+                        Outage: 0,
+                        DownTime: 0,
+         });
+       
       })
       .catch( async(error) => {
         //In case of Failure, the response is Down
         // We can make a Response Interface, Better coding
-        await UpdateDB({UserId: Check.UserId, URL: Check.Url, StatusText: 'down'});
-      })}, 5000);
+        const EndTime = Date.now();
+        const ResponseTime = EndTime-StartTime;
+        await UpdateDB({UserId: Check.UserId,
+                        URL: Check.Url,
+                        StatusText: 'down',
+                        ResponseTime: ResponseTime,
+                        DownTime: ResponseTime / 1000,
+                        Outage: 1,
+                        UpTime:0,
+        }); 
+      })}, 10000);
   })
 }
 
