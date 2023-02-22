80ea556783545ecb3630e8ac647b58c6ac7d63f6
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-22 04:43:34 +0200

    Update Url.js

--- src/models/Url.js
+++ src/models/Url.js
@@ -1,63 +1,65 @@
 import mongoose from 'mongoose';
 
 let UrlSchema = new mongoose.Schema({
-   
-    UserId: {
+  UserId: {
+    type: String,
+    required: [true],
+  },
+
+  Name: {
+    type: String,
+    required: [true],
+  },
+
+  Url: {
+    type: String,
+    required: [true],
+  },
+
+  Path: {
+    type: String,
+  },
+
+  Protocol: {
+    type: String, // Should be Enum
+    required: [true],
+  },
+
+  Port: {
+    type: Number,
+  },
+
+  Timeout: {
+    type: Number,
+    default: 5000, // 5 Seconds
+  },
+
+  Interval: {
+    type: Number,
+    default: 600000, // 10 minutes
+  },
+
+  Threshold: {
+    type: Number,
+    default: 1,
+  },
+
+  Authentication: {
+    type: {
+      username: {
         type: String,
-        required: [true]
-    },
-
-    Name:{
+      },
+      password: {
         type: String,
-        required: [true]
+      },
     },
+  },
 
-    Url:{
-        type: String,
-        required: [true],
-    },
-    
-    Protocol:{
-        type: String, // Should be Enum
-        required: [true],
-    },
+  HttpHeaders: {
+    type: [String],
+  },
 
-    Port:{
-        type: Number,
-    },
-
-    Timeout: {
-        type:Number,
-        default: 5000, // 5 Seconds
-    },
-    
-    Interval:{
-        type: Number,
-        default:600000 // 10 minutes 
-    },
-
-    Threshold:{
-        type: Number,
-        default: 1, 
-    },
-
-    Authentication:{
-        type: { 
-            username:{
-                type: String
-            },
-            password:{
-                type:String
-            }
-        }   
-    },
-    
-    HttpHeaders:{ 
-        type: [String]
-    },
-
-
-    IgnoreSSL: Boolean
-})
+  IgnoreSSL: Boolean,
+});
 UrlSchema = mongoose.model('url', UrlSchema);
-export {UrlSchema};
+export { UrlSchema };
