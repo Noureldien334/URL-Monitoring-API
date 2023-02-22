79b4ddf35d393bcc9718779d01ee27e5fbada8e5
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-22 13:27:48 +0200

    Add files via upload

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,26 +1,41 @@
 import express from 'express';
-import {getUsers,RegisterUser, loginUser} from '../controllers/UserController.js';
-import { getUrls, createUrl, DeleteUrl } from '../controllers/UrlController.js';
-import {Authenticate} from '../middlewares/Auth.js';
-import { GetReports } from '../controllers/ReportController.js';
+import {
+  getUsers,
+  RegisterUser,
+  loginUser,
+} from '../controllers/UserController.js';
+import {
+  GetChecks,
+  CreateCheck,
+  DeleteCheck,
+  UpdateCheck,
+} from '../controllers/CheckController.js';
+import { Authenticate } from '../middlewares/Auth.js';
+import {
+  GetReports,
+  GetReportsByTag,
+} from '../controllers/ReportController.js';
 
 const router = express.Router();
 
 //User Routes
-router.get('/',getUsers)
-      .post('/Register',RegisterUser)
-      .post('/Login',loginUser);
+router
+  .get('/', getUsers)
+  .post('/Register', RegisterUser)
+  .post('/Login', loginUser);
 
 //Url Routes
-router.use(Authenticate)
-      .get('/MyURLs',getUrls)
-      .post('/MyURLs',createUrl)
-      .delete('/DelURL/:URLId',DeleteUrl)   
-//   .put('/:URLId', UpdateUrl)
+router
+  .use(Authenticate)
+  .get('/MyURLs', GetChecks)
+  .post('/MyURLs', CreateCheck)
+  .delete('/DelURL/:URLId', DeleteCheck)
+  .put('/MyURLs/:URLId', UpdateCheck);
 
-//Report Route
-router.use(Authenticate)
-      .route('/Reports')
-      .get(GetReports);
+//Report Routes
+router
+  .use(Authenticate)
+  .get('/Reports', GetReports)
+  .get('/Reports/:Tag', GetReportsByTag);
 
-export {router};
+export { router };
