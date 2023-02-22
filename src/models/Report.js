58ad5e8fb5b17f4ff52228e5c1df143a26e2bef0
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-22 04:43:06 +0200

    Update Report.js

--- src/models/Report.js
+++ src/models/Report.js
@@ -1,25 +1,56 @@
 import mongoose from 'mongoose';
 
 let ReportSchema = mongoose.Schema({
-    URLId: {
-        type: String,
-        required: [true]
-    },
-    Status:{
-        type: String,
-        required: [true]
-    }, // Enumeration,
+  //The Check's Owner
+  CreatorId: {
+    type: String,
+    required: [true],
+  },
+  //Check's URL
+  URL: {
+    type: String,
+    required: [true],
+  },
+  //Current Status of the URL
+  Status: {
+    type: String,
+    required: [true],
+    default: 'Up',
+  }, // Enumeration,
 
-    Availability: {
-        type: Number,
-        required: [true]
-    }, // percentage
+  Availability: {
+    type: Number,
+    default: 0,
+  },
+  //Total number of Downtimes
+  Outage: {
+    type: Number,
+    default: 0,
+  },
+  //Total time of downtimes In SECONDS
+  // Downtime = Down time / Total Time Monitoring
+  DownTime: {
+    type: Number,
+    required: [true],
+    default: 0,
+  },
 
-    Outage: Number, //Total number of Downtimes
-    Downtime: Number, //Total time of downtimes
-    Uptime: Number, //Total time of uptime
-    ResponseTime: Number, // Average Response Time
-    History: [Date], // TimeStamps
-})
+  //Total time of uptim In SECONDS
+  // Uptime = 100% - Down time percentage
+  UpTime: {
+    type: Number,
+    required: [true],
+    default: 0,
+  },
+  // Average Response Time
+  // Response Time = Time taken to complete a GET Request
+  ResponseTime: {
+    type: Number,
+    required: [true],
+    default: 0,
+  },
+
+  History: [String], // TimeStamps
+});
 ReportSchema = mongoose.model('report', ReportSchema);
-export {ReportSchema}
+export { ReportSchema };
