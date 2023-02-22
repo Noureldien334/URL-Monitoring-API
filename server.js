e4130db5a5a17f520f2f6fa13ac13ded6883431d
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-22 13:27:18 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-import express, { application } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import {router} from './src/routes/Routes.js';
-import  bodyParser from 'body-parser';
+import { router } from './src/routes/Routes.js';
+import bodyParser from 'body-parser';
 import { Ping } from './src/pingSched/Pinger.js';
 
-dotenv.config() // Configure .Env File
+dotenv.config(); // Configure .Env File
 
-const app = express()
+const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(router);
 
 const start = () => {
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI);
-    Ping()
-    app.listen(port, () => {
-        console.log("Server is running");
-    })
-}
+  mongoose.set('strictQuery', false);
+  mongoose.connect(process.env.MONGO_URI);
+  Ping();
+  app.listen(port, () => {
+    console.log('Server is running');
+  });
+};
 
 ///////////////////// Make Each Controller as a CLASS////////////////
-start()
+start();
