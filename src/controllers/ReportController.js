93a412c47a701232e2c0ebc00602427b97e03298
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-02-22 04:44:26 +0200

    Update ReportController.js

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -1,12 +1,11 @@
-import { ReportSchema } from "../models/Report.js";
+import { ReportSchema } from '../models/Report.js';
 
-const GetReports = async function(req, res){
-    try{
-        const UserReports = await ReportSchema.find({});
-        res.status(200).json({MyReports: UserReports});
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
-
+const GetReports = async function (req, res) {
+  try {
+    const UserReports = await ReportSchema.find({ CreatorId: req.UserId });
+    res.status(200).json({ MyReports: UserReports });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 export { GetReports };
